b035f7bbcd45680f482e0c5b8c6efbe20ccc0e23
Herivin Simmasalam <hsimma0@gmail.com>
2022-06-14 11:56:25 -0400

    Third Commit Set Up Props Links on Nav  and Template for Dash Main Stock About

---
+++ src/components/Nav.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Nav = (props) => {
+  return (
+    <div className="nav">
+      <Link to="/">
+        <div>iStocks</div>
+      </Link>
+
+      <Link to="/stock">
+        <div>Stock</div>
+      </Link>
+
+      <Link to="/about">
+        <div>About</div>
+      </Link>
+
+    </div>
+  );
+};
+
+export default Nav;
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,7 @@
+import { Link } from "react-router-dom";
+
+const Dashboard = () => {
+    console.log ("This is a placeholder")
+};
+
+export default Dashboard;
