90c16a94f34e6c31f8a9d977eef16e4a95acf3bc
Herivin Simmasalam <hsimma0@gmail.com>
2022-06-14 11:41:01 -0400

    Second Commit App and Index Set Up w Imports and Routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import {Route} from 'react-router-dom';
 import './App.css';
+import Nav from './components/Nav';
+import About from './pages/About';
+import Main from './pages/Main';
+import Stock from './pages/Stock'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Nav />
+      <Route path="/">
+        <Main />
+      </Route>
+
+      <Route path="/stock">
+        <Stock />
+      </Route>
+
+      <Route path="/about">
+        <About />
+      </Route>
+      
     </div>
   );
 }
